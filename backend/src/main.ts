4d8e824008392529288de86bbddf4999a082bf13
santiago portillo <santiagoportillof@gmail.com>
2026-02-02 23:29:17 -0500

    feat(backend): enable CORS for frontend development
    - Added CORS configuration to allow requests from frontend
    - Configured allowed origins (localhost:5173 and 127.0.0.1:5173)
    - Enabled necessary HTTP methods and credentials

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,6 +4,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  // Habilitamos CORS para permitir requests desde el frontend (Localmente)
+  app.enableCors({
+    origin: ['http://localhost:5173', 'http://127.0.0.1:5173'], // puertos mas comunes 
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
